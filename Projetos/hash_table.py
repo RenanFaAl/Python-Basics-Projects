e94e022694b2bfe9a0d08242974bad487fa0078b
Renan Fabiani <renanfabianialmeida@gmail.com>
2026-02-09 21:34:09 -0300

    Hash Table

---
+++ Projetos/hash_table.py
@@ -0,0 +1,37 @@
+class HashTable():
+    def __init__(self):
+        self.collection = {}
+
+    def hash(self, word: str):
+        return sum(ord(char) for char in word)
+    
+    
+    def add(self, key, value):
+        index = self.hash(key)
+        
+        if index not in self.collection:
+            self.collection[index] = {}
+        
+        
+        self.collection[index][key] = value  
+
+
+    def remove(self, key):
+        index = self.hash(key)
+            
+
+        if index in self.collection and key in self.collection[index]:
+            del self.collection[index][key]
+
+            if not self.collection[index]:
+                del self.collection[index]
+    
+    def lookup(self, key):
+        index = self.hash(key)
+
+        if index in self.collection and key in self.collection[index]:
+            return self.collection[index][key]
+        return None
+
+hashT = HashTable()
+print(hashT.hash('cat'))
